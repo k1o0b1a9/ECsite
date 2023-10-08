2580d6b43714f78c3f9f8826448f5957f2761b29
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-10-08 10:45:54 +0900

    first commit

---
+++ ec-backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+
+// MongoDBへの接続（MongoDB AtlasやローカルのMongoDBを使用できます）
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+const PORT = 6000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
